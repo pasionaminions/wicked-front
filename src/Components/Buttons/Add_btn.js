f4e30cae9f038cea5f2bd78615ccefbe822f72f9
Sergi Perelló Campmany <40128966+SergiPerello@users.noreply.github.com>
2020-02-07 10:40:12 +0100

    added add_btn

---
+++ src/Components/Buttons/Add_btn.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Button from 'react-bootstrap/Button'
+
+class Add_btn extends Component {
+    render() {
+        return (
+            <Button variant="primary">Add</Button>
+        );
+    }
+}
+export default Add_btn;
