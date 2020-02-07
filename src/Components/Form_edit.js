9f40d6182082ae9fe04ab07207b311ca4f2328ce
Zakariae Zbakh El Ghoury <zzbakh444@gmail.com>
2020-02-07 10:58:32 +0100

    Form_edit
    Fornulario EDITAR acabado.

--- src/Components/Form_edit.js
+++ src/Components/Form_edit.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { isEmpty } from 'lodash';
 import { Form } from 'reactstrap';
 
 const mapStateToProps = state => {
-
-}
-
-const mapDispatchToProps = {
-
+    return { single: state.single }
 }
 
 class Form_edit extends Component {
@@ -21,6 +18,23 @@ class Form_edit extends Component {
         };
     }
 
+
+    static getDerivedStateFromProps(props, state) {
+        if (!isEmpty(props.single) && !state.state) {
+            return {
+                id: props.single.id,
+                name: props.single.name,
+                email: props.single.email,
+                rate: props.single.rate,
+                color: props.single.color,
+                state: true
+            }
+        }
+        else {
+            return state;
+        }
+    }
+
     handleChange = (event) => {
         this.setState({ [event.target.id]: event.target.value });
     }
@@ -30,25 +44,25 @@ class Form_edit extends Component {
             <Form onChange={this.handleChange}>
                 <FormGroup>
                     <Label for="name">Name:</Label>
-                    <Input type="text" name="name" id="name" value=""/>
+                    <Input type="text" name="name" id="name" value={this.state.name} />
                 </FormGroup>
                 <FormGroup>
                     <Label for="email">Email:</Label>
-                    <Input type="email" name="email" id="email" value="" />
+                    <Input type="email" name="email" id="email" value={this.state.email} />
                 </FormGroup>
                 <FormGroup>
                     <Label for="rate">Rate:</Label>
-                    <Input type="num" name="rate" id="rate" value="" />
+                    <Input type="num" name="rate" id="rate" value={this.state.rate} />
                 </FormGroup>
                 <FormGroup>
                     <Label for="color">Color:</Label>
-                    <Input type="text" name="color" id="color" value="" />
+                    <Input type="text" name="color" id="color" value={this.state.color} />
                 </FormGroup>
             </Form>
         );
     }
 }
 
-const Edit = connect(mapStateToProps, mapDispatchToProps)(Form_edit);
+const Edit = connect(mapStateToProps)(Form_edit);
 
 export default Edit;
