8ac9f7967b33170f7ce8903eb30bd04c253e6c08
Sergi Perelló Campmany <40128966+SergiPerello@users.noreply.github.com>
2020-02-07 10:52:58 +0100

    imporved add button

--- src/Components/Buttons/Add_btn.js
+++ src/Components/Buttons/Add_btn.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+
+const mapDispatchToProps = {
+    post_user
+}
 
 class Add_btn extends Component {
+
+
     render() {
         return (
-            <Button variant="primary">Add</Button>
+            <Button variant="primary" onClick={() => { this.props.post_user() }}>Add</Button>
         );
     }
 }
-export default Add_btn;
+const add_btn = connect(null, mapDispatchToProps)(Add_btn);
+export default add_btn;
