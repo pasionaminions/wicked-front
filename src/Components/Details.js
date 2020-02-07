d7dcd587e370dde0282339b14ac0702dcef8f966
Sergi Perelló Campmany <40128966+SergiPerello@users.noreply.github.com>
2020-02-07 12:02:44 +0100

    updated details page

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { get_single } from "../action/index";
 
+const mapStateToProps = state => {
+    return { obj: state.single }
+}
+
 const mapDispatchToProps = {
     get_single
 }
@@ -16,7 +20,7 @@ class Details extends Component {
             Color: ""
         };
     }
-    componentDidMount(){
+    componentDidMount() {
         this.props.get_single(this.props.params.id);
     }
 
@@ -28,23 +32,23 @@ class Details extends Component {
             <>
                 <div>
                     <label>Name</label>
-                    <input type="text" id="name" onChange={this.handleChange} />
+                    <input type="text" id="name" value={this.props.obj.Name} onChange={this.handleChange} />
                 </div>
                 <div>
                     <label>Email</label>
-                    <input type="text" id="email" onChange={this.handleChange} />
+                    <input type="text" id="email" value={this.props.obj.Email} onChange={this.handleChange} />
                 </div>
                 <div>
                     <label>Rate</label>
-                    <input type="text" id="rate" onChange={this.handleChange} />
+                    <input type="text" id="rate" value={this.props.obj.Rate} onChange={this.handleChange} />
                 </div>
                 <div>
                     <label>Color</label>
-                    <input type="text" id="color" onChange={this.handleChange} />
+                    <input type="text" id="color" value={this.props.obj.Color} onChange={this.handleChange} />
                 </div>
             </>
         );
     }
 }
-const details = connect(null, mapDispatchToProps)(Details);
+const details = connect(mapStateToProps, mapDispatchToProps)(Details);
 export default details;
