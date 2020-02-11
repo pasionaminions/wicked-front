ff6dfc50f28462fee2a1c142683ade652a357b59
AdriRed <adriamilmarcos@gmail.com>
2020-02-11 10:00:53 +0100

    Finished form

--- src/Components/Form_edit.js
+++ src/Components/Form_edit.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { isEmpty } from 'lodash';
-import { get_single } from "../thunks";
-import { Form, FormGroup, Label, Input } from 'reactstrap';
+import { get_single, post_user, edit_user } from "../thunks";
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 
 const mapStateToProps = state => {
     return { single: state.single }
 }
 
 const mapDispatchToProps = {
-    get_single
+    get_single, post_user, edit_user
 }
 
 class Form_edit extends Component {
@@ -21,8 +21,7 @@ class Form_edit extends Component {
             name: "",
             email: "",
             rate: 0,
-            color: "",
-            loaded: this.new
+            color: ""
         };
     }
 
@@ -43,7 +42,7 @@ class Form_edit extends Component {
 
     componentDidMount() {
         if (this.new) return;
-        
+
         this.props.get_single(this.props.id)
         .then(() => this.setState(() => {
             return {name: this.props.single.name,
@@ -53,29 +52,43 @@ class Form_edit extends Component {
         }));
     }
 
+    save = () => {
+        let obj = this.state;
+        obj.rate = parseInt(obj.rate);
+        let prom;
+        if (this.new) {
+            prom = this.props.post_user(obj);
+        } else {
+            obj.id = parseInt(this.props.id);
+            prom = this.props.edit_user(obj.id, obj);
+        }
+        prom.then(() => this.props.history.push("/"));
+    }
+
     handleChange = (event) => {
         this.setState({ [event.target.id]: event.target.value });
     }
 
     render() {
         return (
-            <Form onChange={this.handleChange}>
+            <Form>
                 <FormGroup>
                     <Label for="name">Name:</Label>
-                    <Input type="text" name="name" id="name" value={this.state.name} />
+                    <Input type="text" name="name" id="name" value={this.state.name} onChange={this.handleChange} />
                 </FormGroup>
                 <FormGroup>
                     <Label for="email">Email:</Label>
-                    <Input type="email" name="email" id="email" value={this.state.email} />
+                    <Input type="email" name="email" id="email" value={this.state.email} onChange={this.handleChange}/>
                 </FormGroup>
                 <FormGroup>
                     <Label for="rate">Rate:</Label>
-                    <Input type="num" name="rate" id="rate" value={this.state.rate} />
+                    <Input type="num" name="rate" id="rate" value={this.state.rate} onChange={this.handleChange}/>
                 </FormGroup>
                 <FormGroup>
                     <Label for="color">Color:</Label>
-                    <Input type="text" name="color" id="color" value={this.state.color} />
+                    <Input type="text" name="color" id="color" value={this.state.color} onChange={this.handleChange}/>
                 </FormGroup>
+                <Button onClick={this.save}>SAVE ELEMENT</Button>
             </Form>
         );
     }
